70e5b60d93df4c966351c927796047d717388a92
saadatovaaijan@icloud.com <saadatovaaijan@icloud.com>
2024-06-13 16:25:19 +0300

    Initial Commit

---
+++ Lesson2.5month/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2.5month
+//
+//  Created by Aijan Saadatova on 13/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
